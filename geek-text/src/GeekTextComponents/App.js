aa0c1a06cfaaf32f9e3159f0f0a7a53d3183c499
awor001 <awors001@fiu.edu>
2019-01-29 16:13:39 -0500

    int. commit
    First commit

--- geek-text/src/GeekTextComponents/App.js
+++ geek-text/src/GeekTextComponents/App.js
@@ -1,10 +1,20 @@
 import React, {Component} from 'react';
 import SearchPage from './SearchPage';
+//import {Helmet} from 'react-helmet';
+//import HomePage from './HomePage'; didn't work
+//class Search extends Component {
+  //It didn't even come close
+  //
+//Why not React.Component
 
 class App extends Component {
+    //Need to make a home Page
     render() {
         return (
              <div>
+                 <h1 align = "center">Geek Text</h1>
+                 
+                     <style>{'body { background-color: light-blue;}'}</style>
                 <SearchPage></SearchPage>
              </div>
         );
--- geek-text/src/index.js
+++ geek-text/src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from "./GeekTextComponents/App";
 import * as serviceWorker from './serviceWorker';
 
+const element =<h1>Geek Text</h1>;
+ReactDOM.render(element, document.getElementById('root'));
+//React.createElement
 ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
