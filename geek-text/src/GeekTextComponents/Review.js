ccf90136e4da6c0af01ea1650873847d3f8ee61f
jwats057 <46846829+jwats057@users.noreply.github.com>
2019-01-31 17:38:01 -0500

    Create Review.js
    Barebones Review component added

---
+++ geek-text/src/GeekTextComponents/Review.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+class Review extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Create a review</h1>
+        <div>
+          <form>
+            <select>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+            <br />
+            <textarea />
+            <br />
+            <button>Submit</button>
+          </form>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Review;
