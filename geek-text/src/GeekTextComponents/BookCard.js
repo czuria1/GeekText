51e835d9932ec4d7e17f302c3f1ae9c82d6184fa
jyepe <yepejy@gmail.com>
2019-01-31 00:20:37 -0500

    Server connects to db and retrives info

---
+++ geek-text/src/GeekTextComponents/BookCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const BookCard = (props) => 
+{
+    return(
+        <div>
+           <h2>"kkk"</h2>
+            
+        </div>
+    )
+}
+
+export default BookCard;
