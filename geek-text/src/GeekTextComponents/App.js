2e46d9d5c0349c57f94ceaf92fc89131b99db73a
jyepe <yepejy@gmail.com>
2019-02-01 01:25:46 -0500

    Parse return string JSON object

--- geek-text/src/GeekTextComponents/App.js
+++ geek-text/src/GeekTextComponents/App.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import SearchPage from './SearchPage';
+import ShoppingCartIcon from "./ShoppingCartIcon";
 
 class App extends Component {
     render() {
         return (
              <div>
                 <SearchPage></SearchPage>
+                <ShoppingCartIcon></ShoppingCartIcon>
              </div>
         );
     }
