0cb578da996ca79cc0b8361fc739ad95715bae85
jyepe <yepejy@gmail.com>
2019-01-31 19:22:34 -0500

    Redirect to Shopping cart page fixed

---
+++ geek-text/src/GeekTextComponents/ShoppingCartPage.js
@@ -0,0 +1,21 @@
+import React, {Component} from "react";
+
+
+class ShoppingCartPage extends Component {
+
+    
+    render() {
+        return (
+             <div className="shopping-cart">
+                <h1>Shopping Cart</h1>
+                <ul>
+                <li>BOOK 1</li>
+                <li>BOOK 2</li>
+                <li>BOOK 3</li>
+                </ul>
+             </div>
+        );
+    }
+}
+
+export default ShoppingCartPage;
