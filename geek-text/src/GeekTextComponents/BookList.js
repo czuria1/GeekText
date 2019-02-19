b24567b1d374283c8c26d13981dca161c369fa0f
jyepe <yepejy@gmail.com>
2019-02-19 17:23:58 -0500

    Book cover enlarged when clicked

--- geek-text/src/GeekTextComponents/BookList.js
+++ geek-text/src/GeekTextComponents/BookList.js
@@ -34,8 +34,64 @@ class BookList extends Component{
 
     appendHTMLElements (bookInfoArray, props) 
     {
+        var modalDiv = document.createElement('div');
+        modalDiv.id = "myModal";
+        modalDiv.className = "modal";
+        modalDiv.style.display = "none";
+        modalDiv.style.position = "fixed";
+        modalDiv.style.zIndex = "1";
+        modalDiv.style.paddingTop = "100px";
+        modalDiv.style.left = "0";
+        modalDiv.style.top = "0";
+        modalDiv.style.width = "100%";
+        modalDiv.style.height = "100%";
+        modalDiv.style.overflow = "auto";
+        modalDiv.style.backgroundColor = "rgb(0,0,0)";
+        modalDiv.style.backgroundColor = "rgb(0,0,0.9)";
+
+        var close = document.createElement('span');
+        close.className = "close";
+        close.appendChild(document.createTextNode("&times;"));
+        close.style.position = "absolute";
+        close.style.top = "15px";
+        close.style.right = "35px";
+        close.style.color = "#f1f1f1";
+        close.style.fontSize = "40px";
+        close.style.fontWeight = "bold";
+        close.style.transition = "0.3s";
+
+        var modalImage = document.createElement('img');
+        modalImage.className = "modal-content";
+        modalImage.id = "img01";
+        modalImage.style.margin = "auto";
+        modalImage.style.display = "block";
+        modalImage.style.width = "80%";
+        modalImage.style.maxWidth = "700px";
+        modalImage.style.animationName = "zoom";
+        modalImage.style.animationDuration = "0.6s";
+
+        var caption = document.createElement('div');
+        caption.id = "caption";
+        caption.style.margin = "auto";
+        caption.style.display = "block";
+        caption.style.width = "80%";
+        caption.style.maxWidth = "700px";
+        caption.style.textAlign = "center";
+        caption.style.color = "#ccc";
+        caption.style.padding = "10px 0";
+        caption.style.height = "150px";
+        caption.style.animationName = "zoom";
+        caption.style.animationDuration = "0.6s";
+
+        modalDiv.appendChild(close);
+        modalDiv.appendChild(modalImage);
+        modalDiv.appendChild(caption);
+
+
         var list = document.createElement('ul');
         list.style.listStyleType = "none"
+
+        list.appendChild(modalDiv);
 
         for (let index = 0; index < bookInfoArray.length; index++) 
         {
@@ -58,6 +114,20 @@ class BookList extends Component{
             addToWishList.href = "";
             cover.src = bookInfoArray[index].split('`')[7];
             cover.alt = "Image not available";
+            cover.id = "cover";
+            cover.style.borderRadius = "5px";
+            cover.style.cursor = "pointer";
+            cover.style.transition = "0.3s"
+            cover.onclick = function() {
+                modalDiv.style.display = "block";
+                modalImage.src = cover.src;
+                caption.innerHTML = "inner";
+            }
+
+            close.onclick = function() {
+                modalDiv.style.display = "none";
+            }
+            
             this.wishListClicked(addToWishList);
 
             // Set its contents:
@@ -113,6 +183,8 @@ class BookList extends Component{
         {
             document.getElementById("book-info-container").appendChild(list);
         }
+
+
 
         return (
              <div>
