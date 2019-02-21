3473ca1e531a7edf8ca190e0da85988914e4d099
jyepe <yepejy@gmail.com>
2019-02-21 18:31:46 -0500

    Update BookList.js

--- geek-text/src/GeekTextComponents/BookList.js
+++ geek-text/src/GeekTextComponents/BookList.js
@@ -46,49 +46,39 @@ class BookList extends Component{
 
         for (let index = 0; index < bookInfoArray.length; index++) 
         {
-            
             //Create list elements
-            var cover = document.createElement('img');
-            this.setCoverAttributes(cover, bookInfoArray, index, modalDiv, modalImage, caption);
-            var title = document.createElement('li');
-            var authorContainer = document.createElement('li');
-            var span = document.createElement('span');
-            var authorLink = document.createElement('a');
-            var genre = document.createElement('li');
-            var publisher = document.createElement('li');
-            var pub_date = document.createElement('li');
-            var description = document.createElement('li');
-            var rating = document.createElement('li');
-            var shoppingCartLink = document.createElement('a');
-            shoppingCartLink.href = "";
-            this.shoppingCartClicked(shoppingCartLink);
-            var line = document.createElement('hr');
+            var { line, 
+                  shoppingCartLink, 
+                  authorLink, 
+                  title, 
+                  authorContainer, 
+                  span, 
+                  genre, 
+                  publisher, 
+                  pub_date, 
+                  description, 
+                  rating, 
+                  cover } = this.createElements(bookInfoArray, index, modalDiv, modalImage, caption);
 
-            line.id = "line";
-            shoppingCartLink.id = "shoppingCartLink";
-            authorLink.id = "authorLink";
-
-            
-            
+            this.addElementId(line, shoppingCartLink, authorLink);
 
             close.onclick = function() {
                 modalDiv.style.display = "none";
             }
             
-            
-            
-
             // Set its contents:
-            title.appendChild(document.createTextNode("Title: " + bookInfoArray[index].split('`')[0]));
-            authorContainer.appendChild(span);
-            span.appendChild(authorLink);
-            authorLink.appendChild(document.createTextNode("Author: " + bookInfoArray[index].split('`')[1]));
-            genre.appendChild(document.createTextNode("Genre: " + bookInfoArray[index].split('`')[2]));
-            publisher.appendChild(document.createTextNode("Publisher: " + bookInfoArray[index].split('`')[3]));
-            pub_date.appendChild(document.createTextNode("Publish Date: " + bookInfoArray[index].split('`')[4]));
-            description.appendChild(document.createTextNode("Description: " + bookInfoArray[index].split('`')[5]));
-            rating.appendChild(document.createTextNode("Rating: " + bookInfoArray[index].split('`')[6]));
-            shoppingCartLink.appendChild(document.createTextNode("Add to shopping cart"));
+            this.setElementContents(title, 
+                                    bookInfoArray, 
+                                    index, 
+                                    authorContainer, 
+                                    span, 
+                                    authorLink, 
+                                    genre, 
+                                    publisher, 
+                                    pub_date, 
+                                    description, 
+                                    rating, 
+                                    shoppingCartLink);
             
             //Pass the author name to the author page
             var authorPageLinkString = authorLink.innerText;
@@ -96,28 +86,76 @@ class BookList extends Component{
             authorLink.href = "#/authorPage/" + `${authorPageLinkString}`; 
 
             // Add it to the list:
-            list.appendChild(cover);
-            list.appendChild(title);
-            
-            if (!props.linkClicked)
-            {
-                list.appendChild(authorContainer);
-            }
-            
-            list.appendChild(genre);
-            list.appendChild(publisher);
-            list.appendChild(pub_date);
-            list.appendChild(description);
-            list.appendChild(rating);
-            list.appendChild(shoppingCartLink);
-            list.appendChild(line);
-                
+            this.appendChildren(list, 
+                                cover, 
+                                title, 
+                                props, 
+                                authorContainer, 
+                                genre, 
+                                publisher, 
+                                pub_date, 
+                                description, 
+                                rating, 
+                                shoppingCartLink, 
+                                line);
         }
 
         return list;
-        
     }
     
+    appendChildren(list, cover, title, props, authorContainer, genre, publisher, pub_date, description, rating, shoppingCartLink, line) {
+        list.appendChild(cover);
+        list.appendChild(title);
+        if (!props.linkClicked) {
+            list.appendChild(authorContainer);
+        }
+        list.appendChild(genre);
+        list.appendChild(publisher);
+        list.appendChild(pub_date);
+        list.appendChild(description);
+        list.appendChild(rating);
+        list.appendChild(shoppingCartLink);
+        list.appendChild(line);
+    }
+
+    setElementContents(title, bookInfoArray, index, authorContainer, span, authorLink, genre, publisher, pub_date, description, rating, shoppingCartLink) {
+        title.appendChild(document.createTextNode("Title: " + bookInfoArray[index].split('`')[0]));
+        authorContainer.appendChild(span);
+        span.appendChild(authorLink);
+        authorLink.appendChild(document.createTextNode("Author: " + bookInfoArray[index].split('`')[1]));
+        genre.appendChild(document.createTextNode("Genre: " + bookInfoArray[index].split('`')[2]));
+        publisher.appendChild(document.createTextNode("Publisher: " + bookInfoArray[index].split('`')[3]));
+        pub_date.appendChild(document.createTextNode("Publish Date: " + bookInfoArray[index].split('`')[4]));
+        description.appendChild(document.createTextNode("Description: " + bookInfoArray[index].split('`')[5]));
+        rating.appendChild(document.createTextNode("Rating: " + bookInfoArray[index].split('`')[6]));
+        shoppingCartLink.appendChild(document.createTextNode("Add to shopping cart"));
+    }
+
+    addElementId(line, shoppingCartLink, authorLink) {
+        line.id = "line";
+        shoppingCartLink.id = "shoppingCartLink";
+        authorLink.id = "authorLink";
+    }
+
+    createElements(bookInfoArray, index, modalDiv, modalImage, caption) {
+        var cover = document.createElement('img');
+        this.setCoverAttributes(cover, bookInfoArray, index, modalDiv, modalImage, caption);
+        var title = document.createElement('li');
+        var authorContainer = document.createElement('li');
+        var span = document.createElement('span');
+        var authorLink = document.createElement('a');
+        var genre = document.createElement('li');
+        var publisher = document.createElement('li');
+        var pub_date = document.createElement('li');
+        var description = document.createElement('li');
+        var rating = document.createElement('li');
+        var shoppingCartLink = document.createElement('a');
+        shoppingCartLink.href = "";
+        this.shoppingCartClicked(shoppingCartLink);
+        var line = document.createElement('hr');
+        return { line, shoppingCartLink, authorLink, title, authorContainer, span, genre, publisher, pub_date, description, rating, cover };
+    }
+
     setCoverAttributes(cover, bookInfoArray, index, modalDiv, modalImage, caption) {
         cover.src = bookInfoArray[index].split('`')[7];
         cover.alt = "Image not available";
