5ac1318631d92c47fc8ada2b56ec93d1109ce578
awor001 <awors001@fiu.edu>
2019-03-15 19:19:41 -0400

    Someone deleted searchpage
    Search page is no longer in master the team decided to go another way with this. I wasn't aware of the situation but now I have my code running. I need to restart everything over.

--- geek-text/src/App.js
+++ geek-text/src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-function dropFunction(){
-  document.getElementById("down").classList.toggle("show");
-}
+
+
+
+
 class App extends Component {
   render() {
     return (
--- geek-text/src/GeekTextComponents/App.js
+++ geek-text/src/GeekTextComponents/App.js
@@ -1,26 +1,134 @@
 import React, {Component} from 'react';
-import SearchPage from './SearchPage'
+
 import Header from './Header'
-import LoginScreen from "./LoginScreen";
-import ShoppingCartPage from "./ShoppingCartPage";
+
+//import LoginScreen from "./ProfileManagement/LoginScreen";
+
+//import RegistrationScreen from './ProfileManagement/RegistrationScreen';
+
+//import ShoppingCartPage from "./ShoppingCartPage";
+
 import {Route, HashRouter} from "react-router-dom";
 
+//import AuthorPage from './AuthorPage';
+
+//import ProfileSettings from './ProfileManagement/ProfileSettings';
+
+import BookList from './BookList';
+
+import SearchArea from './SearchArea';
+
+
+
 class App extends Component {
+
+
+
+    constructor (props) {
+
+        super (props);
+
+        this.state = {
+
+            currentUser: 'test',
+
+            isUserLoggedIn: false
+
+        }
+
+
+
+        this.setCurrentUser = this.setCurrentUser.bind(this);
+
+        this.logoutUser = this.logoutUser.bind(this);
+
+
+
+        document.title = "Geek Text";
+
+    }
+
+
+
+    setCurrentUser(newUser, loggedIn) {
+
+        this.setState({
+
+            currentUser: newUser,
+
+            isUserLoggedIn: loggedIn
+
+        });
+
+        
+
+    }
+
+
+
+    logoutUser(oldUser, loggedIn) {
+
+        this.setState({
+
+            currentUser: oldUser,
+
+            isUserLoggedIn: loggedIn
+
+        });
+
+    }
+
+
+
     render() {
+
+      
+
         return (
+
             <HashRouter>
+
                 <div>
-                    <Header></Header>
+
+                    <Header 
+
+                        currentUser={this.state.currentUser}
+
+                        isUserLoggedIn={this.state.isUserLoggedIn}
+
+                        logoutUser={this.state.logoutUser}></Header>
+
+
 
                     <div>
-                        <Route path="/search" component={SearchPage}/>
-                        <Route path="/login" component={LoginScreen}/>
-                        <Route path="/shoppingCart" component={ShoppingCartPage}/>
+
+                      <div id="route-container">
+
+                        <Route path="/search" component={SearchArea}/>
+
+
+                        
+
+                       
+
+                   
+
+                        <Route path="/bookList/:term" component={BookList}/>
+
+                     </div>
+
                     </div>
+
                 </div>
+
             </HashRouter>
+
         );
+
     }
+
 }
+
+
 
 export default App;
