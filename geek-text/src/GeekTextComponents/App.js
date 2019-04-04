35af53e5f2f7012bb3f161fe58ccadfbfffc8973
czuria1 <czuria1@gmail.com>
2019-04-04 19:35:39 -0400

    Fix App.js error

--- geek-text/src/GeekTextComponents/App.js
+++ geek-text/src/GeekTextComponents/App.js
@@ -18,7 +18,7 @@ class App extends Component {
     constructor (props) {
         super (props);
         this.state = {
-            currentUser: 'john',
+            currentUser: '',
             currentUserId: '',
             isUserLoggedIn: false
         }
