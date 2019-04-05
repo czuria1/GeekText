b0ea6828587b6248a622c33b5a3145efb4802216
jyepe <yepejy@gmail.com>
2019-04-05 17:38:11 -0400

    update

--- geek-text/src/GeekTextComponents/Display Book Info Components/BookDetails.js
+++ geek-text/src/GeekTextComponents/Display Book Info Components/BookDetails.js
@@ -7,10 +7,6 @@ import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Button from '@material-ui/core/Button';
 import { Image } from 'react-bootstrap';
-<<<<<<< HEAD
-import ajaxme from "ajaxme";
-// import StarsRating from 'stars-rating'
-=======
 import StarsRating from 'stars-rating';
 import {Link as RouterLink} from 'react-router-dom';
 import ServerCall from "../ServerCall";
@@ -23,7 +19,6 @@ const BookCover = styled(Image)`
 const Heading = styled.h3`
     padding-top: 100px; 
 `;
->>>>>>> Julian
 
 class BookDetails extends Component {
     constructor(props) {
@@ -61,11 +56,7 @@ class BookDetails extends Component {
         {
             var reviewList = this.state.reviews.map(function(review, index){
                 return <div key={index} id="reviewContainer">
-<<<<<<< HEAD
-                            {/* <StarsRating count={5} value={review.rating} size={30} edit={false}></StarsRating> */}
-=======
                             <StarsRating count={5} value={parseInt(review.rating, 10)} size={30} edit={false}></StarsRating>
->>>>>>> Julian
                             <span>{review.comment}</span>
                             <p></p>
                             <span>By: {review.username}</span>
