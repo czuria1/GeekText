89b3c9b2dee4c0c4ffafa7207ced2d36d3675600
awor001 <awors001@fiu.edu>
2019-04-12 14:37:23 -0400

    TRYING CONNECT
    erroe

--- geek-text/server.php
+++ geek-text/server.php
@@ -3,8 +3,8 @@
 	
     //Info to connect to DB
 	$servername = "localhost";
-	$dbusername = "jyepe";
-	$dbpassword = "9373yepe";
+	$dbusername = "awors";
+	$dbpassword = "May1993!";
 	$dbname = "geektext_db";
 
 	//what method to execute
--- geek-text/src/GeekTextComponents/Display Book Info Components/BookList.js
+++ geek-text/src/GeekTextComponents/Display Book Info Components/BookList.js
@@ -4,6 +4,13 @@ import List from "./List";
 import ModalCover from "./ModalCover";
 import ServerCall from "../ServerCall";
 import styled from "styled-components";
+import ajaxme from "ajaxme";
+import SearchArea from "./SearchArea";
+// import Pagination from "react-js-pagination";
+// import 'rc-pagination/assets/index.css';`
+
+const pageSize = 10;
+
 
 const ListContainer = styled.div`
     width: 60%;
@@ -19,21 +26,48 @@ class BookList extends Component {
     constructor(props) {
         super(props);
         this.state = { 
-            books: [] 
+            books: [],
+            fi:[],
+            onPage: 1,
+            order: "ASC",
+            pageSize:10,
+            //js-pagination
+            apageSize:10,
+            itemsCountPerPage: 10,
+            totalItemsCount: 10,
+            sort: "title"
         };
         this.retriveResults = this.retriveResults.bind(this);
         this.returnList = this.returnList.bind(this);
         this.showNoResults = this.showNoResults.bind(this);
         this.changeState = this.changeState.bind(this);
+        this.ASC = this.ASC.bind(this);
+        this.DESC = this.DESC.bind(this);
     }
 
     componentDidMount() {
         this.retriveResults(this.props.match.params.term);
+        if(this.props.pageSize){
+            this.setState({size: this.props.pageSize});
+          }
+    }
+   
+
+    
+    onChangePage = (page) =>{
+        console.log(Math.ceil(this.state.books.length/pageSize));
+        this.setState({
+            onPage: page,
+        });
+
     }
 
     shouldComponentUpdate(nextProps, nextState) {
-        if (nextProps.match.params.term === this.props.match.params.term && this.state.books.length === nextState.books.length)
+        console.log(nextState);
+        console.log(this.state);
+        if (this.props.match.params.term === nextProps.match.params.term && this.state.books.length === nextState.books.length && nextState.order === this.state.order)
         {
+            
             return false;
         }
         else
@@ -41,6 +75,8 @@ class BookList extends Component {
             this.retriveResults(nextProps.match.params.term);
             return true;
         }
+       
+        
     }
 
     changeState(response) {
@@ -48,9 +84,26 @@ class BookList extends Component {
             books: response
         });
     }
+   
+
+   
+   
+
+    DESC(event){
+        console.log(event.target.value);
+        this.setState({order: event.target.value});
+    }
+    
+    ASC(event){
+        console.log(event.target.value);
+        this.setState({order: event.target.value});
+    }
+
+    
 
     retriveResults(term) {
-        ServerCall("getSearchInfo", term, this.changeState);
+        console.log(this.state.order);
+        ServerCall("getSearchInfo", term + ";"+ this.state.order, this.changeState);
     }
 
     returnList() {
@@ -78,13 +131,46 @@ class BookList extends Component {
     }
 
     render() { 
+      //  this.retriveResults();
         return ( 
+
             <div>
                 {this.showNoResults()}
                 <ListContainer>
                     <ModalCover></ModalCover>
                     {this.returnList()}
                 </ListContainer>
+                <p>ORDER THE BOOKS</p>
+            <select defaultValue = {this.state.order} onChange = {this.ASC}>
+            <option value ={"ASC"} onClick = {this.ASC} >DESCENDING</option>
+            <option value ={"DESC"}onClick = {this.DESC} >ASCENDING</option>
+            </select>
+            
+            <div id="listContainer">
+           
+            </div>
+        
+            {/* <Pagination class = "d-flex justify-content-center"
+activePage={this.state.activePage}
+itemsCountPerPage={10}
+totalItemsCount={450}
+pageRangeDisplayed={5}
+onChange={this.handlePageChange}
+itemClass='page-item'
+linkClass= 'page-link'
+//  onChangePage = {this.onChangePage}
+                        
+                        // onChange={onChange}
+                        
+                        // onPage = {this.state.onPage}
+                        // pageSize = {this.state.pageSize}
+                        // pageSizeOptions = {'10,20'}
+                        // showQuickJumper
+                        // showSizeChanger
+                        total = {this.state.books.length}
+                        
+
+                        />  */}
             </div>
          );
     }
