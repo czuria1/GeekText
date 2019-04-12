155fb39fb5f3d592ce0eabcf4cf89482811870d7
czuria1 <czuria1@gmail.com>
2019-04-12 19:34:49 -0400

    Small change to App.js

--- geek-text/src/GeekTextComponents/App.js
+++ geek-text/src/GeekTextComponents/App.js
@@ -19,10 +19,10 @@ class App extends Component {
     constructor (props) {
         super (props);
         this.state = {
-            currentUser: 'testUser',
-            isUserLoggedIn: true,
-            userID: '1',
-            currentUserHomeAddressId: '1' 
+            currentUser: '',
+            isUserLoggedIn: false,
+            userID: '',
+            currentUserHomeAddressId: '' 
         }
 
         this.setCurrentUser = this.setCurrentUser.bind(this);
